4240121591a24339543f54fea8786f9d25efe174
Miguel Sanchez <mangelsr25@gmail.com>
2025-06-18 10:32:08 -0500

    [ADD] Missing exercises

--- Assets/Modulo8/Script/EjerciciosVariablesMod8.cs
+++ Assets/Modulo8/Script/EjerciciosVariablesMod8.cs
@@ -34,6 +34,49 @@ public class EjerciciosVariablesMod8 : MonoBehaviour
             Debug.Log($"item: {item}");
         }
 
+        /*
+        Crea dos variables string que guarden valores numéricos de miles, pasalas a tipos de datos 
+        numéricos utilizando la función tryParse, realiza una operación con ellas e imprime el resultado.
+        */
+        string number1 = "19972506";
+        string number2 = "10300017";
+        float n1, n2;
+
+        float.TryParse(number1, out n1);
+        float.TryParse(number2, out n2);
+
+        float result = n1 / n2;
+
+        Debug.Log($"result: {result}");
+
+
+        /*
+        Crea un string con una oración, imprime sólo los caracteres que se encuentren en un índice par, 
+        por ejemplo “Hola Mundo”, solo deberá imprimir los caracteres: H,l, ,u,d. 
+        (recuerda para qué sirve el uso del operador módulo).
+        */
+        string sentence = "I'm learning Unity in EBAC";
+        string evenChars = "";
+        for (int i = 0; i < sentence.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                evenChars += $"{sentence[i]},";
+            }
+        }
+        evenChars.Substring(0, evenChars.Length - 1);
+        Debug.Log($"evenChars: {evenChars}");
+
+
+        /*
+        Crea un string guarda, una oración en él y elimina los primeros 5 caracteres de este 
+        ya sea con la función substring, Split,  o cualquier otro método disponible en la clase String
+        */
+        string lastSentence = "This is the last exercise of the homework";
+        string reducedString = lastSentence.Substring(5, lastSentence.Length - 5);
+        Debug.Log($"lastSentence: {lastSentence}");
+        Debug.Log($"reducedString: {reducedString}");
+
         mesh = gameObject.GetComponent<MeshRenderer>();
     }
 
